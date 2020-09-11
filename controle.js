894e9bfade6fe18a099bd781ace4589113396b9f
yasminee123 <50171355+yasminee123@users.noreply.github.com>
2020-09-11 12:22:32 +0200

    version1

---
+++ controle.js
@@ -0,0 +1,32 @@
+//Obje JSON :c'est un type de données contenat des attibuts(firstname,lastname et age).Il s'agit pas d'un nouveau type
+/*var obj={
+    "firstname":"foulen",
+    "lastname":"benfoulen",
+    "age":52
+}
+//Affichage des attributs d'un objet JSON
+console.log(obj.age)
+console.log(obj["age"])*/
+
+//Array(peut contenir des élements qui n'ont pas le meme type)
+/*var users=['mouna','hadil','yasmine','amine','maram']
+console.log(users[2])
+users.push('malek')
+users.splice(3,1)
+console.log(users)
+users.sort()
+console.log(users)*/
+
+/*var users=['mouna','hadil','yasmine','amine','maram']
+document.write("<ul>")
+for(var user of users)
+    document.write("<li>",user,"</li>")
+document.write("</ul>")*/
+
+//var comme variable golbale et let locale ya3ni ki tokhrej mel bloc eli heya fih ma3adech visible w const constante
+// git est un outil de versionnig nekhdmou bih localment  3al pc
+//github orienté projets
+
+//terminal -> git init (iwali yepointi ala el dossier li ena nheb naamalou versionnage w n9olou arja3 lel version el foulaneya)
+//U :  c'est un nouveau fichier non connu par git 
+// je veux que git connait mes fichies : git add nom fichier ou git add . ( barra t3aref ala ay fichier jdid)
